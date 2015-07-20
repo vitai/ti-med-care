c514054e6ad3bce45ceb60470763bb6b219e0215
Leandro Mussi da Silva <leandromussi@ti-med.net>
2015-07-20 21:11:19 +0000

    Changes made by Leandro Mussi da Silva (leandromussi@ti-med.net).

--- Definicao/scripts/app.js
+++ Definicao/scripts/app.js
@@ -5,23 +5,36 @@
     app.usuarioSettings = [];
     app.permissoes = null;
     app.currentViewModel = null;
-    app.TotalUnidades = 2;
+    app.unidadeCorrente = null;
    
-    app.Login = function(userSettings){
-        if(app.TotalUnidades == 1){
-        app.usuarioSettings = userSettings;
-        app.permissoes = userSettings.PERMISSOES;
-        console.log(userSettings);  
-        app.application.navigate('views/Menu.html');      
-        }else{
-        app.usuarioSettings = userSettings;
-        app.permissoes = userSettings.PERMISSOES;
-        console.log(userSettings);  
-        app.application.navigate('views/unidadesView.html');        
-
-        }
-    }
+app.Login = function(userSettings){
+var permissao = null;
     
+for (var i = 0; i < userSettings.UNIDADES.length; i++) {
+           if (userSettings.UNIDADES[i] !== undefined) {
+                        ++permissao;
+                        console.log(permissao);
+            }
+           if(permissao <= 1){
+            app.usuarioSettings = userSettings;
+            app.permissoes = userSettings.PERMISSOES;
+            app.unidadeCorrente = userSettings.UNIDADES[0];
+            app.unidadeUrl = userSettings.UNIDADES[0].URL;
+            console.log(userSettings);
+            console.log(app.unidadeUrl);
+            console.log(app.unidadeCorrente);    
+            app.application.navigate('views/Menu.html');      
+            }else{
+            app.usuarioSettings = userSettings;
+            app.permissoes = userSettings.PERMISSOES;
+            console.log(userSettings);
+            app.application.navigate('views/unidadesView.html');    
+            }
+         
+        }
+      
+     }
+
 document.addEventListener("deviceready",function(){
         navigator.splashscreen.hide();
             document.addEventListener("resume", function(){
@@ -31,7 +44,6 @@ document.addEventListener("deviceready",function(){
             }, false);
     
         kendo.culture("pt-BR"); 
-        app.unidadeCorrente = null;
         app.currentViewModel = null;
     
  app.application = new kendo.mobile.Application(document.body, {skin: 'flat', initial: 'views/login.html'});
