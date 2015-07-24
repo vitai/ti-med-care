32974205c4b93e7b9a8d06d8fd9923349aecfdeb
Leandro Mussi da Silva <leandromussi@ti-med.net>
2015-07-24 19:40:40 +0000

    Changes made by Leandro Mussi da Silva (leandromussi@ti-med.net).

--- Definicao/scripts/Menu.js
+++ Definicao/scripts/Menu.js
@@ -1,10 +1,13 @@
 (function(global){
    var Menus,
-    app = global.app = global.app || {};
+    app = global.app  || {};
     
     Menus = kendo.data.ObservableObject.extend({
-        onViewShow: function(e)
-        {
+         dataAtualizacao: "",
+         descricaoUnidade: "",
+        onViewShow: function(e) {
+            var that = this;
+            that.set("descricaoUnidade", app.unidadeCorrente.DESCRICAO);
             this.dataSource.read({ data: app.unidadeCorrente.MENU});     
         },
         dataSource: new kendo.data.DataSource({
@@ -12,14 +15,19 @@
                 read: function(operation) {
                 var data = operation.data.data || [];
                 operation.success(data);
-                }
-            }            
-        })   
-      });
+           		     }
+                }                
+ 	      }),
+        onUpdate: function() 
+        {
+         
+           this.refresh();
+        }
+    });
+ 
     
     app.MenuService  = {
        viewModel : new Menus()
     }; 
   
 })(window);
-
