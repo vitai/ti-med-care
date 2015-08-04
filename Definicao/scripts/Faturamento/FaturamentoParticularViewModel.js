0226bdf757f3ac178f646a000f042cf2878dfc88
Leandro Mussi da Silva <leandromussi@ti-med.net>
2015-08-04 22:05:28 +0000

    Changes made by Leandro Mussi da Silva (leandromussi@ti-med.net).

--- Definicao/scripts/Faturamento/FaturamentoParticularViewModel.js
+++ Definicao/scripts/Faturamento/FaturamentoParticularViewModel.js
@@ -4,6 +4,17 @@
 
     app.unidadeUrl = "http://santacasadecampos.dyndns.org:8080/sits/";
     
+    app.mes = null;
+    app.ano = null;
+    app.ExibirMes = null;
+    app.ExibirMesAnterior = null;
+    app.ultimoDiadoMes = null;
+    app.MesFaturado = null; 
+    
+    app.mesAnterior = null;
+    app.anoAnterior = null;
+    app.ultimoDiadoMesAnterior = null; 
+    
     faturamentoParticularViewModel = kendo.data.ObservableObject.extend({
         ValorTotal:0,
         onInit: function(e) {
@@ -13,7 +24,40 @@
         onBeforeShowView: function(e) {
             this.dataSource.transport.options.read.url = app.unidadeUrl + "ws/relatorio";
             this.dataSourceConsolidado.transport.options.read.url = app.unidadeUrl + "ws/relatorio";
-            this.refresh();   
+            this.refresh();  
+            app.mes = kendo.toString(new Date(), "MM");
+            app.ano = kendo.toString(new Date(), "yyyy");
+            app.mesAnterior  = kendo.toString(new Date(app.ano, app.mes - 2, 1), "MM"); 
+            app.ultimoDiadoMes = (new Date(app.ano, app.mes, 0 )).getDate();
+            
+            //Exibir os meses em string mes atual / mes anterior
+            app.ExibirMes = kendo.toString(new Date(), "MMMM"); // Mes Atual 
+            app.ExibirMesAnterior = kendo.toString(new Date(app.ano, app.mes - 2, 1), "MMMM"); // Mes Anterior
+            app.MesFaturado = kendo.toString(new Date(), "MMMM");
+             
+            this.set("ExibirMes", app.ExibirMes);
+            this.set("ExibirMesAnterior", app.ExibirMesAnterior);  
+            this.set("MesFaturado", app.MesFaturado); 
+
+            //Tratamento para exibir o mes do anterior quando o mes for Janeiro.
+            if (app.mes == 1){
+               console.log(app.mes); 
+                app.mesAnterior = 12;
+                app.anoAnterior = app.ano - 1;
+                app.ultimoDiadoMesAnterior = (new Date(app.anoAnterior, app.mesAnterior, 0 )).getDate();    
+            }
+            else {
+                app.mesAnterior = app.mes - 1;
+                app.anoAnterior = app.ano;
+                app.ultimoDiadoMesAnterior = (new Date(app.anoAnterior, app.mesAnterior, 0 )).getDate();    
+                
+            }
+            
+            console.log(app.ExibirMesAnterior);
+            console.log(app.ExibirMes);
+            console.log(app.mes);
+            console.log(app.ano);
+            console.log(app.ultimoDiadoMes); 
         },   
         refresh: function() {
             this.dataSource.read();
@@ -30,8 +74,8 @@
                       var param = {
                           "q":32,
                           "setorId": 1,
-                          "dataInicial": kendo.toString(new Date(2015, 7, 1), "G"),
-                          "dataFinal": kendo.toString(new Date(), "G")
+                          "dataInicial": kendo.toString(new Date(app.ano, app.mes -1, 1), "G"),
+                          "dataFinal": kendo.toString(new Date(app.ano, app.mes - 1, app.ultimoDiadoMes), "G")
 
                       };
                       
@@ -58,8 +102,8 @@
                       var param = {
                           "q":33,
                           "setorId": 1,
-                          "dataInicial": kendo.toString(new Date(2015, 7, 1), "G"),
-                          "dataFinal": kendo.toString(new Date(), "G")
+                          "dataInicial": kendo.toString(new Date(app.ano, app.mes -1, 1), "G"),
+                          "dataFinal": kendo.toString(new Date(app.ano, app.mes - 1, app.ultimoDiadoMes), "G")
 
                       };
                       
